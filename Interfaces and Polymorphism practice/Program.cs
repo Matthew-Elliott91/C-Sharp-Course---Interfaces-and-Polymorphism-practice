581a25f5e682adf442d317487b5b8fc7d2db9fd8
Matthew Elliott <m.elliott091091@gmail.com>
2025-01-05 18:46:31 +0000

    Add project files.

---
+++ Interfaces and Polymorphism practice/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Interfaces_and_Polymorphism_practice
+{
+    internal class Program
+    {
+        public interface IAnimal
+        {
+            void MakeSound();
+            void Eat(string food);
+
+        }
+
+        public class Dog : IAnimal
+        {
+            public void Eat(string food)
+            {
+                Console.WriteLine($"Dog ate the {food} ");
+            }
+
+            public void MakeSound()
+            {
+                Console.WriteLine("Bark");
+            }
+        }
+
+        public class Cat : IAnimal
+        {
+            public void Eat(string food)
+            {
+                Console.WriteLine($"Cat ate the {food} ");
+            }
+            public void MakeSound()
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.MakeSound();
+            dog.Eat("Treat");
+        }
+    }
+}
